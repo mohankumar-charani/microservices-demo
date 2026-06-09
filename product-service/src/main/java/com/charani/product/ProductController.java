98a12584919d9ba1a7d0abcf4928373195e2361a
Mohan Kumar <mohankumar.charani@gmail.com>
2026-06-09 14:00:02 +0530

    initial commit

---
+++ product-service/src/main/java/com/charani/product/ProductController.java
@@ -0,0 +1,15 @@
+package com.charani.product;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String products() {
+        return "Product Service Running";
+    }
+}
